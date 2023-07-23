6c129c88b612ce0ba8cc57c4e6fcc6f83ac51e8e
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-07-23 16:57:52 +0900

    feat: Angularのapplicationを追加
    ng generate application web

--- scripts.sh
+++ scripts.sh
@@ -1,2 +1,3 @@
 ng new miyako-de-it-hackathon-v1 --commit false --create-application false --inline-style --inline-template --package-manager npm --routing --skip-git --standalone --strict --style css
 cd miyako-de-it-hackathon-v1
+ng generate application web --inline-style --inline-template --routing --standalone --strict --style css
