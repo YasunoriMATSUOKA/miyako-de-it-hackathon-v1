d25074f3a70b98a302a8aac864599030c379bf52
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2023-07-23 16:50:06 +0900

    feat: Angularで初期化
    ng new

---
+++ scripts.sh
@@ -0,0 +1,2 @@
+ng new miyako-de-it-hackathon-v1 --commit false --create-application false --inline-style --inline-template --package-manager npm --routing --skip-git --standalone --strict --style css
+cd miyako-de-it-hackathon-v1
